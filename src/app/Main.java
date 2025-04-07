9a239c17c35aeb4a6491c977a66d32537ec66d9c
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-04-08 00:45:07 +0300

    Add miles to km conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
 public class Main {
+
+    public static final double KM_COEFFICIENT = 1.621;
+
     public static void main(String[] args) {
         System.out.println("App for miles to kilometers conversion and vice versa");
+
+        double miles = 10;
+        double kilometers = convertMilesToKm(miles);
+        System.out.printf("%.3f miles are equivalent to %.3f kilometers\n", miles, kilometers);
+    }
+    public static double convertMilesToKm(double miles) {
+        return miles * KM_COEFFICIENT;
     }
 }
