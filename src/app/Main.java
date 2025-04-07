5985034eb27e2d663a65afc32982b1149efac45b
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-04-08 00:46:39 +0300

    Add km to miles conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,8 +10,15 @@ public class Main {
         double miles = 10;
         double kilometers = convertMilesToKm(miles);
         System.out.printf("%.3f miles are equivalent to %.3f kilometers\n", miles, kilometers);
+
+        miles = convertKmToMiles(kilometers);
+        System.out.printf("%.3f kilometers are equivalent to %.3f miles\n", kilometers, miles);
     }
     public static double convertMilesToKm(double miles) {
         return miles * KM_COEFFICIENT;
+    }
+
+    public static double convertKmToMiles(double km) {
+        return km / KM_COEFFICIENT;
     }
 }
